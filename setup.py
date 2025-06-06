d0ec5addb6578b0875d1767c174a3ddc7d9eba75
Justin Faulk <j.faulk@me.com>
2025-06-06 13:12:08 -0400

    testing

--- setup.py
+++ setup.py
@@ -19,15 +19,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Multimedia :: Video",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.13",
     install_requires=[
-        "telnetlib3>=2.0.0",
+        "telnetlib-313-and-up",
     ],
 )
